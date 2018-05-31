4ddd6424c0a7664f2c99e1225d7b1f4285729c0d
Juan Andres Rueda <jrueda@frro.utn.edu.ar>
2018-05-31 10:21:22 -0300

    Hecho hasta punto 3 del Lab03

---
+++ Entidades/BusinessEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class BusinessEntity
+    {
+        public BusinessEntity()
+        {
+            this.State = States.New;
+        }
+
+        public enum States
+        {
+            Deleted, New, Modified, Unmodified
+        }
+
+        private int id;
+        private States state;
+
+        public int Id { get => Id; set => Id = value; }
+        public States State { get => state; set => state = value; }
+    }
+
+}
