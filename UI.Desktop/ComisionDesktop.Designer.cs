c0927b77c258deeddb9b91a9b65dcb29188dec54
gui2_ <gui2_@LAPTOP-9CMT2CQQ>
2018-09-05 15:45:29 -0300

    nuevoCommit

--- UI.Desktop/ComisionDesktop.Designer.cs
+++ UI.Desktop/ComisionDesktop.Designer.cs
@@ -29,11 +29,176 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblAño = new System.Windows.Forms.Label();
+            this.lblPlan = new System.Windows.Forms.Label();
+            this.cmbPlan = new System.Windows.Forms.ComboBox();
+            this.planesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.planesBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.66666F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 83F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+            this.tableLayoutPanel1.Controls.Add(this.btnAceptar, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnSalir, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblID, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblDescripcion, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblAño, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblPlan, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cmbPlan, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtDescripcion, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtID, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtAño, 3, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(454, 116);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(213, 83);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(296, 83);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(3, 13);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 2;
+            this.lblID.Text = "ID";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(3, 53);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 3;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // lblAño
+            // 
+            this.lblAño.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblAño.AutoSize = true;
+            this.lblAño.Location = new System.Drawing.Point(213, 13);
+            this.lblAño.Name = "lblAño";
+            this.lblAño.Size = new System.Drawing.Size(26, 13);
+            this.lblAño.TabIndex = 4;
+            this.lblAño.Text = "Año";
+            // 
+            // lblPlan
+            // 
+            this.lblPlan.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblPlan.AutoSize = true;
+            this.lblPlan.Location = new System.Drawing.Point(213, 53);
+            this.lblPlan.Name = "lblPlan";
+            this.lblPlan.Size = new System.Drawing.Size(28, 13);
+            this.lblPlan.TabIndex = 5;
+            this.lblPlan.Text = "Plan";
+            // 
+            // cmbPlan
+            // 
+            this.cmbPlan.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.cmbPlan.DataSource = this.planesBindingSource;
+            this.cmbPlan.DisplayMember = "desc_plan";
+            this.cmbPlan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPlan.FormattingEnabled = true;
+            this.cmbPlan.Location = new System.Drawing.Point(296, 49);
+            this.cmbPlan.Name = "cmbPlan";
+            this.cmbPlan.Size = new System.Drawing.Size(121, 21);
+            this.cmbPlan.TabIndex = 6;
+            this.cmbPlan.ValueMember = "id_plan";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtDescripcion.Location = new System.Drawing.Point(73, 50);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(121, 20);
+            this.txtDescripcion.TabIndex = 7;
+            // 
+            // txtID
+            // 
+            this.txtID.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtID.Location = new System.Drawing.Point(73, 10);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(121, 20);
+            this.txtID.TabIndex = 9;
+            // 
+            // txtAño
+            // 
+            this.txtAño.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtAño.Location = new System.Drawing.Point(296, 10);
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(121, 20);
+            this.txtAño.TabIndex = 10;
+            // 
+            // ComisionDesktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(454, 116);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ComisionDesktop";
             this.Text = "ComisionDesktop";
+            this.Load += new System.EventHandler(this.ComisionDesktop_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.planesBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblAño;
+        private System.Windows.Forms.Label lblPlan;
+        private System.Windows.Forms.ComboBox cmbPlan;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.BindingSource planesBindingSource;
+        private System.Windows.Forms.TextBox txtAño;
     }
 }
--- UI.Desktop/ComisionDesktop.cs
+++ UI.Desktop/ComisionDesktop.cs
@@ -7,14 +7,153 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Entidades;
+using Negocio;
 
 namespace UI.Desktop
 {
-    public partial class ComisionDesktop : Form
+    public partial class ComisionDesktop : ApplicationForm
     {
         public ComisionDesktop()
         {
             InitializeComponent();
+        }
+
+        private void ComisionDesktop_Load(object sender, EventArgs e)
+        {
+            PlanesLogic pl = new PlanesLogic();
+            cmbPlan.DataSource = pl.GetAll();
+            cmbPlan.DisplayMember = "Descripcion";
+            cmbPlan.ValueMember = "ID";
+        }
+        public enum ModoForm
+        {
+            Alta, Baja, Modificación, Consulta
+        }
+        public ModoForm Modo { get; set; }
+        public virtual void MapearDeDatos()
+        {
+            this.txtID.Text = this.ComisionActual.ID.ToString();
+            this.txtDescripcion.Text = this.ComisionActual.Descripcion;
+            this.txtAño.Text = this.ComisionActual.AñoEspecialidad.ToString();
+            this.cmbPlan.SelectedValue = this.ComisionActual.IdPlan;
+            switch (Modo)
+            {
+                case ModoForm.Alta:
+                case ModoForm.Modificación:
+                    {
+                        this.btnAceptar.Text = "Guardar";
+                        break;
+                    }
+                case ModoForm.Baja:
+                    {
+                        this.btnAceptar.Text = "Eliminar";
+                        txtAño.Enabled = false;
+                        txtDescripcion.Enabled = false;
+                        cmbPlan.Enabled = false;
+                        break;
+                    }
+                case ModoForm.Consulta:
+                    {
+                        this.btnAceptar.Text = "Aceptar";
+                        break;
+                    }
+            }
+        }
+        public virtual void MapearADatos()
+        {
+            if (Modo == ModoForm.Alta)
+            {
+                Business.Entities.Comision c = new Comision();
+                ComisionActual = c;
+                this.ComisionActual.State = BusinessEntity.States.New;
+            }
+            if (Modo == ModoForm.Modificación)
+            {
+                this.ComisionActual.ID = int.Parse(this.txtID.Text);
+                this.ComisionActual.State = BusinessEntity.States.Modified;
+            }
+            this.ComisionActual.Descripcion = this.txtDescripcion.Text;
+            this.ComisionActual.IdPlan = (int)this.cmbPlan.SelectedValue;
+            this.ComisionActual.AñoEspecialidad = int.Parse(this.txtAño.Text);
+        }
+        public virtual void GuardarCambios()
+        {
+            Business.Logic.ComisionLogic cl = new ComisionLogic();
+            switch (btnAceptar.Text)
+            {
+                case "Aceptar":
+                case "Guardar":
+                    {
+                        MapearADatos();
+                        cl.Save(ComisionActual);
+                        break;
+                    }
+                case "Eliminar":
+                    {
+                        cl.Delete(ComisionActual.ID);
+                        break;
+                    }
+            }
+        }
+        public virtual bool Validar()
+        {
+            if (String.IsNullOrWhiteSpace(txtDescripcion.Text))
+            {
+                Notificar("Campo vacío", "La descripción no puede estar vacía", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+            else if (Util.Validacion.ValidarNum(txtAño.Text))
+            {
+                Notificar("Año Incorrecto", "El año debe ser un número entero", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+            else
+                return true;
+        }
+        public void Notificar(string titulo, string mensaje, MessageBoxButtons botones, MessageBoxIcon icono)
+        {
+            MessageBox.Show(mensaje, titulo, botones, icono);
+        }
+        public void Notificar(string mensaje, MessageBoxButtons botones, MessageBoxIcon icono)
+        {
+            this.Notificar(this.Text, mensaje, botones, icono);
+        }
+        public Business.Entities.Comision ComisionActual { get; set; }
+        public ComisionDesktop(ModoForm modo)
+            : this()
+        {
+            Modo = ModoForm.Alta;
+        }
+        public ComisionDesktop(int ID, ModoForm modo)
+            : this()
+        {
+            Modo = modo;
+            Business.Logic.ComisionLogic cl = new ComisionLogic();
+            ComisionActual = cl.GetOne(ID);
+            MapearDeDatos();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (this.btnAceptar.Text == "Eliminar")
+            {
+                GuardarCambios();
+                Close();
+            }
+            else
+            {
+                if (Validar())
+                {
+                    GuardarCambios();
+                    Close();
+                }
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
