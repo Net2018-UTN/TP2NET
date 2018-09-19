08af890f99c06f3803358d673aa34a4725816557
Juan Andres Rueda <jrueda@frro.utn.edu.ar>
2018-09-19 11:38:23 -0300

    Capa datos cursos hecha

---
+++ Entidades/Curso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Curso : BusinessEntity
+    {
+        int idMateria;
+        int idComision;
+        int anioCalendario;
+        int cupo;
+
+        public int IdCurso { get => idCurso; set => idCurso = value; }
+        public int IdMateria { get => idMateria; set => idMateria = value; }
+        public int IdComision { get => idComision; set => idComision = value; }
+        public int AnioCalendario { get => anioCalendario; set => anioCalendario = value; }
+        public int Cupo { get => cupo; set => cupo = value; }
+    }
+}
