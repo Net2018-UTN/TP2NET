f064e6213cfd97426c6949678338a8f7a35a2fc9
Juan Andres Rueda <jrueda@frro.utn.edu.ar>
2018-09-27 11:38:22 -0300

    Hicimos el login pero no tiene menu, solo se loguea

---
+++ UI.Desktop/Login.cs
@@ -0,0 +1,105 @@
+﻿using Entidades;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        UsuarioLogic ul = new UsuarioLogic();
+
+        /*public UsuarioLogic Ul
+        {
+            get
+            {
+                if (Ul == null)
+                {
+                    ul = new UsuarioLogic();
+                }
+                return ul;
+            }
+        }*/
+
+        Usuario usuario;
+
+        private Usuario Usuario { get; set; }
+
+        PersonasLogic pl;
+        public PersonasLogic Pl
+        {
+            get
+            {
+                if (Pl == null)
+                {
+                    pl = new PersonasLogic();
+                }
+                return pl;
+            }
+        }
+
+        private Persona persona { get; set; }
+
+        private void formLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                usuario = ul.GetUsuario(txtUsuario.Text);
+                if(usuario != null)
+                {
+                    if(this.ValidarPass(usuario.Clave)){
+                        this.DialogResult = DialogResult.OK;
+                    }
+
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+
+            //A esto hay que adaptarlo para que funcione con la capa de negocio
+
+
+            /*if (this.txtUsuario.Text == Ul.get && this.txtPass.Text == "admin")
+            {
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña incorrectos", "Login"
+                    , MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+       }
+
+        private bool ValidarPass(string clave)
+        {
+            if (this.txtPass.Text.Equals(clave))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private void lnkOlvidaPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Es Ud. un usuario muy descuidado, haga memoria", "Olvidé mi contraseña",
+                   MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
---
+++ UI.Desktop/Menu.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace UI.Desktop
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.mnuArchivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuPrincipal
+            // 
+            this.mnuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuArchivo});
+            this.mnuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.mnuPrincipal.Name = "mnuPrincipal";
+            this.mnuPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.mnuPrincipal.TabIndex = 1;
+            this.mnuPrincipal.Text = "menuStrip1";
+            // 
+            // mnuArchivo
+            // 
+            this.mnuArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSalir});
+            this.mnuArchivo.Name = "mnuArchivo";
+            this.mnuArchivo.Size = new System.Drawing.Size(60, 20);
+            this.mnuArchivo.Text = "Archivo";
+            // 
+            // mnuSalir
+            // 
+            this.mnuSalir.Name = "mnuSalir";
+            this.mnuSalir.Size = new System.Drawing.Size(180, 22);
+            this.mnuSalir.Text = "Salir";
+            this.mnuSalir.Click += new System.EventHandler(this.mnuSalir_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mnuPrincipal);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnuPrincipal;
+            this.Name = "Menu";
+            this.Text = "Academia";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Shown += new System.EventHandler(this.Menu_Shown);
+            this.mnuPrincipal.ResumeLayout(false);
+            this.mnuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem mnuArchivo;
+        private System.Windows.Forms.ToolStripMenuItem mnuSalir;
+    }
+}
